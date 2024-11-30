2b671010e34026c9474334846bd6f65d0927bd4a
ammar-jesliy <ammarjc1@gmail.com>
2024-11-30 17:56:51 +0530

    feat: add getter and setter for ID

--- src/main/java/lk/ac/iit/eventticketingbackend/model/Ticket.java
+++ src/main/java/lk/ac/iit/eventticketingbackend/model/Ticket.java
@@ -20,6 +20,14 @@ public class Ticket {
     public Ticket() {
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getTicketNumber() {
         return ticketNumber;
     }
