f4649012acfadc4173afd06f20bca8d2134b5c63
ammar-jesliy <ammarjc1@gmail.com>
2024-12-04 17:11:45 +0530

    feat: add getTransactionByVendorId function

--- src/main/java/lk/ac/iit/eventticketingbackend/repository/TransactionRepository.java
+++ src/main/java/lk/ac/iit/eventticketingbackend/repository/TransactionRepository.java
@@ -3,5 +3,8 @@ package lk.ac.iit.eventticketingbackend.repository;
 import lk.ac.iit.eventticketingbackend.model.Transaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    List<Transaction> findTransactionsByVendorId(String vendorId);
 }
