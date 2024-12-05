35304ae3bbf765b1fdf906428ef5b6687ac53c86
ammar-jesliy <ammarjc1@gmail.com>
2024-12-05 13:32:25 +0530

    feat: add find tickets by vendor id

--- src/main/java/lk/ac/iit/eventticketingbackend/repository/TicketRepository.java
+++ src/main/java/lk/ac/iit/eventticketingbackend/repository/TicketRepository.java
@@ -3,5 +3,8 @@ package lk.ac.iit.eventticketingbackend.repository;
 import lk.ac.iit.eventticketingbackend.model.Ticket;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends MongoRepository<Ticket, String> {
+    List<Ticket> findTicketsByVendorId(String vendorId);
 }
