35ff674cb7bb247ead16a42451d5a1f3c7db75dc
ammar-jesliy <ammarjc1@gmail.com>
2024-12-08 12:30:36 +0530

    feat: add transient attributes for simulation and add run method
    adds transient attributes for simulations parameters
    implements runnable interface
    adds run method to sell tickets

--- src/main/java/lk/ac/iit/eventticketingbackend/model/Vendor.java
+++ src/main/java/lk/ac/iit/eventticketingbackend/model/Vendor.java
@@ -2,11 +2,14 @@ package lk.ac.iit.eventticketingbackend.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Document
-public class Vendor {
+public class Vendor implements Runnable {
     @Id
     @JsonProperty("id")
     private String id;
@@ -14,7 +17,14 @@ public class Vendor {
     @Indexed(unique = true)
     private String email;
     private String password;
+    private LocalDateTime dateCreated;
+
+    @Transient
     private int releaseRate;
+    @Transient
+    private TicketPool ticketPool;
+    @Transient
+    private int amount;
 
     public Vendor() {
     }
@@ -24,6 +34,30 @@ public class Vendor {
         this.email = email;
         this.password = password;
         this.releaseRate = releaseRate;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            Ticket ticket = new Ticket();
+            ticket.setPrice(100);
+            ticket.setVendorId(id);
+            ticket.setEventId(ticket.getEventId());
+            ticket.setAvailable(true);
+
+            boolean success = ticketPool.addTicket(ticket);
+
+            if (!success) {
+                System.out.println("Ticket Pool capacity has reached. Cannot release any more tickets");
+                break;
+            }
+
+            try {
+                Thread.sleep(releaseRate * 1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
     }
 
     public String getName() {
@@ -50,12 +84,44 @@ public class Vendor {
         this.password = password;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
     public int getReleaseRate() {
         return releaseRate;
     }
 
     public void setReleaseRate(int releaseRate) {
         this.releaseRate = releaseRate;
+    }
+
+    public TicketPool getTicketPool() {
+        return ticketPool;
+    }
+
+    public void setTicketPool(TicketPool ticketPool) {
+        this.ticketPool = ticketPool;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 
     @Override
